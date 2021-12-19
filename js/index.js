aa82f4cf866d76b06cc974685d20db422775c935
Alejo <alejoso@opendeusto.es>
2021-12-19 16:56:46 +0100

    nuevo formato

--- js/index.js
+++ js/index.js
@@ -1,90 +1,86 @@
 $(document).ready(function () {
-    $('#telefono').mask('000-000-0000');
+	$('#telefono').mask('000-000-0000');
 });
-
 let txtNombre = document.getElementById('txtNombre');
 txtNombre.focus();
 
 const app = new Vue({
-    el: '#app',
-    data: {
-        contactos: [],
-        contactoNombre: '',
-        contactoEmail: '',
-        contactoTelefono: '',
-        seleccionado: false,
-    },
-    methods: {
-        limpiar: function () {
-            this.contactoNombre = '';
-            this.contactoEmail = '';
-            this.contactoTelefono = '';
-        },
+	el: '#app',
+	data: {
+		titulo: 'Agenda Vue',
+		contactos: [],
+		contactoNombre: '',
+		contactoTelefono: '',
+		contactoInsta: '',
+		seleccionado: false,
+	},
+	methods: {
+		limpiar: function () {
+			this.contactoNombre = '';
+			this.contactoTelefono = '';
+			this.contactoEmail = '';
+		},
+		// -- AGREGAR --
+		agregarContacto: function () {
+			//Validaciones
+			if (
+				this.contactoNombre === '' ||
+				this.contactoTelefono === '' ||
+				this.contactoEmail === ''
+			) {
+				alert('¡Todos los campos son requeridos!');
+			} else {
+				this.contactos.push({
+					nombre: this.contactoNombre,
+					telefono: this.contactoTelefono,
+					insta: this.contactoEmail,
+				});
 
-        agregarContacto: function () {
-            if (
-                this.contactoNombre === '' ||
-                this.contactoEmail === '' ||
-                this.contactoTelefono === ''
-                
-            ) {
-                alert('Tienes que rellenar todos los campos.');
-            } else {
-                this.contactos.push({
-                    nombre: this.contactoNombre,
-                    email: this.contactoEmail,
-                    telefono: this.contactoTelefono,
-                });
+				this.limpiar();
+				localStorage.setItem('agenda-vue', JSON.stringify(this.contactos));
+				let txtNombre = document.getElementById('txtNombre');
+				txtNombre.focus();
+			}
+		},
+		// -- MOSTRAR --
+		mostrarContacto: function (index) {
+			if (this.contactoNombre === '') {
+				document.getElementById('boton-submit').disabled = true;
+				this.contactos[index].seleccionado = true;
+				this.contactoNombre = this.contactos[index].nombre;
+				this.contactoTelefono = this.contactos[index].telefono;
+				this.contactoEmail = this.contactos[index].email;
+			} else {
+				this.editarContacto(index);
+				this.contactos[index].seleccionado = false;
+				document.getElementById('boton-submit').disabled = false;
+			}
+		},
+		// -- EDITAR --
+		editarContacto: function (index) {
+			this.contactos[index].nombre = this.contactoNombre;
+			this.contactos[index].telefono = this.contactoTelefono;
+			this.contactos[index].email = this.contactoEmail;
+			this.btnEdicion = 'Editar';
+			this.limpiar();
 
-                this.limpiar();
-                localStorage.setItem('agenda-vue', JSON.stringify(this.contactos));
-                let txtNombre = document.getElementById('txtNombre');
-                txtNombre.focus();
-            }
-        },
+			localStorage.setItem('agenda-vue', JSON.stringify(this.contactos));
+		},
+		// -- ELIMINAR --
+		eliminar: function (index) {
+			this.contactos.splice(index, 1);
+			localStorage.setItem('agenda-vue', JSON.stringify(this.contactos));
+		},
+	},
+	// -- GUARDAR EN LOCAL --
+	created: function () {
+		let datosDB = JSON.parse(localStorage.getItem('agenda-vue'));
+		console.log(datosDB);
 
-
-        mostrarContacto: function (index) {
-            if (this.contactoNombre === '') {
-                document.getElementById('boton-submit').disabled = true;
-                this.contactos[index].seleccionado = true;
-                this.contactoNombre = this.contactos[index].nombre;
-                this.contactoTelefono = this.contactos[index].telefono;
-                this.contactoEmail = this.contactos[index].email;
-            } else {
-                this.editarContacto(index);
-                this.contactos[index].seleccionado = false;
-                document.getElementById('boton-submit').disabled = false;
-            }
-        },
-
-
-        editarContacto: function (index) {
-            this.contactos[index].nombre = this.contactoNombre;
-            this.contactos[index].email = this.contactoEmail;
-            this.contactos[index].telefono = this.contactoTelefono;
-            this.btnEdicion = 'Editar';
-            this.limpiar();
-
-            localStorage.setItem('agenda-contactos', JSON.stringify(this.contactos));
-        },
-
-
-        eliminar: function (index) {
-            this.contactos.splice(index, 1);
-            localStorage.setItem('agenda-contactos', JSON.stringify(this.contactos));
-        },
-    },
- 
-
-    created: function () {
-        let datosDB = JSON.parse(localStorage.getItem('agenda-contactos'));
-        console.log(datosDB);
-
-        if (datosDB === null) {
-            this.contactos = [];
-        } else {
-            this.contactos = datosDB;
-        }
-    },
+		if (datosDB === null) {
+			this.contactos = [];
+		} else {
+			this.contactos = datosDB;
+		}
+	},
 });
