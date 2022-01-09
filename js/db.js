1eb4997cbc953ed2729a29fdb07e569a3d9a5f83
Alejo <alejoso@opendeusto.es>
2022-01-09 18:15:46 +0100

    intento firebase 1

---
+++ js/db.js
@@ -0,0 +1,28 @@
+import './firebase';
+
+import Vue from 'vue';
+import App from './App.vue';
+import VueFire from 'vuefire';
+import { namesref } from './firebase';
+
+Vue.use(VueFire);
+
+new Vue({
+    el: '#app',
+    render: h => h(app)
+})
+
+export default {
+    data(){
+        return{
+            nombre: '',
+            telefono: '',
+            email: ''
+        }
+    },
+    methods: {
+        enviarContacto(){
+            namesref.push({nombre: this.nombre, telefono: this.telefono, email: this.email})
+        }
+    }
+}
